afdfdefc4991582699c79493422c58417c790f95
Will Hoover <70659810+LordGuacaba@users.noreply.github.com>
2024-02-26 13:57:34 -0500

    more tests

--- backend/tests/test_chatterbot.py
+++ backend/tests/test_chatterbot.py
@@ -3,10 +3,21 @@ General tests for the chatterbot Ramsay AI to ensure it responds appropriately a
 of our training.
 """
 
-from main import read_recipes
+from main import read_recipes, Chatbot
+
+def setup():
+    bot = Chatbot()
+    bot.train(read_recipes("backend/data/tworecipes.txt"))
+    return bot
 
 def test_read_recipes():
     recipes = read_recipes("backend/data/tworecipes.txt")
 
     assert len(recipes) == 4
     assert recipes[0] == "Can you give me a recipe for beef Wellington?"
+
+def test_basic_response():
+    bot = setup()
+    response = bot.respond("How do you properly cook a perfect ribeye steak?")
+    
+    assert response == "Seriously? Sear it on high heat and let it rest before slicing!"

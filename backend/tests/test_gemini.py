035201b4895a2b98681096b7a864ed3b7cad6fba
Brandon Montijo <bcm8504@rit.edu>
2024-03-18 09:47:57 -0400

    Update tests

--- backend/tests/test_gemini.py
+++ backend/tests/test_gemini.py
@@ -3,25 +3,29 @@ General tests for the chatterbot Ramsay AI to ensure it responds appropriately a
 of our training.
 """
 
-# from gemini import Chatbot
+from gemini import Chatbot
 
-# def test_responses():
-#     bot = setup()
 
-#     response = bot.respond("How do you make a creamy risotto?")
-#     assert response != ""
+def setup():
+    return Chatbot()
 
-# def test_chat_history():
-#     bot = setup()
-#     response = bot.respond("How do you make a creamy risotto?")
-#     assert bot.chat_history[0] == "How do you make a creamy risotto?"
-#     assert bot.chat_history[1] == response
 
-# def setup():
-#     return Chatbot()
-#
-# def test_precise_responses():
-#     bot = setup()
-#
-#     response = bot.respond("How do you make a creamy risotto?")
-#     assert response != ""
+def test_responses():
+    bot = setup()
+
+    response = bot.respond("How do you make a creamy risotto?")
+    assert response != ""
+
+
+def test_chat_history():
+    bot = setup()
+    response = bot.respond("How do you make a creamy risotto?")
+    assert bot.chat_history[0] == "How do you make a creamy risotto?"
+    assert bot.chat_history[1] == response
+
+
+def test_precise_responses():
+    bot = setup()
+
+    response = bot.respond("How do you make a creamy risotto?")
+    assert response != ""
